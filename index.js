5939192c3e303c024ab9f5d857fca515c77cdad2
justice sowah <justice.sowah18@gmail.com>
2021-06-26 20:43:17 +0000

    specific route middleware created for the routes

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const express = require('express');
 //defining request handlers
 
 const handleGetLoginRequest = (req, res) => {
-    res.send("Welcome! Login Sucessful! Enjoy your stay!");
+    res.send("<h1>Welcome! Login Sucessful! Enjoy your stay!</h1>");
 }
 
 const handlePostSignUpRequest = (req, res) => {
-    res.send("Voila! SignUp Sucessful! You are now a member of this site!");
+    res.send("<h1>Voila! SignUp Sucessful! You are now a member of this site!</h1>");
 }
 
 const handlePatchProfileRequest = (req, res) => {
-    res.send("Congrats! Your profile is updated successfully!");
+    res.send("<h1>Congrats! Your profile is updated successfully!</h1>");
 }
 
 const handleDeleteProfileRequest = (req, res) => {
-    res.send("Your profile deleted successfully! We are sorry to see you go :(");
+    res.send("<h1>Your profile deleted successfully! We are sorry to see you go :( </h1>");
 }
 //creating express server
 const server = express();
@@ -26,24 +26,39 @@ const server = express();
 //general middleware
 
 /* const handleAllRequest = (req, res, next) => {
-    res.send('this is a general middleware');
+    res.send('<h1>this is a general middleware</h1>');
     next();
 } */
 
 //route specific middleware
 
 const handleLoginRequest = (req, res, next) => {
-    console.log('this is a route specific request for the login route');
+    console.log('this is a route specific middleware that calls the handleGetLoginRequest request handler for the login route');
     next();
 }
 
-//server.use(handleAllRequest);
+const handlePostRequest = (req, res, next) => {
+    console.log('this is a route specific middleware that calls the handlePostSignUpRequest request handler for the signup route');
+    next();
+}
+
+const handlePatchRequest = (req, res, next) => {
+    console.log('this is a route specific middleware that calls the handlePatchProfileRequest request handler for the profile route');
+    next();
+}
+
+const handledeleteRequest = (req, res, next) => {
+    console.log('this is a route specific middleware that calls the handleDeleteProfileRequest request handler to delete the profile on the profile route');
+    next();
+}
+
+// server.use(handleAllRequest);
 
 //routes
 server.get('/login', handleLoginRequest, handleGetLoginRequest);
-server.post('/signup', handlePostSignUpRequest);
-server.patch('/profile', handlePatchProfileRequest);
-server.delete('/profile', handleDeleteProfileRequest);
+server.post('/signup', handlePostRequest, handlePostSignUpRequest);
+server.patch('/profile', handlePatchRequest, handlePatchProfileRequest);
+server.delete('/profile', handledeleteRequest, handleDeleteProfileRequest);
 
 //starting the server
 server.listen(3000, () => console.log('server is up and running'))
